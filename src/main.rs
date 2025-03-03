195a1da458a35fe3734d5f7b9df8d04859ffba86
Huaiyuan Jing <ls.hyjing@gmail.com>
2025-03-03 13:27:29 -0600

    refactor(main): comment out test_complete_graph function

--- src/main.rs
+++ src/main.rs
@@ -14,24 +14,24 @@ fn main() {
     println!("collision rate is {}%", (grundy_cache.collision_rate() - 1.0) * 100.0);
 }
 
-fn test_complete_graph(n: usize, grundy_cache: &mut grundy_cache::GrundyCache) {
-    let mut nodes = Vec::new();
-    let mut graph: Graph<(), (), petgraph::Undirected> = Graph::new_undirected();
-    for _ in 0..n {
-        let node = graph.add_node(());
-        nodes.push(node);
-    }
-    for i in 0..n - 1 {
-        for j in i + 1..n {
-            graph.add_edge(nodes[i], nodes[j], ());
-        }
-    }
-    println!(
-        "grundy of complete graph of size {} is {}",
-        n,
-        grundy::grundy(&graph, grundy_cache)
-    );
-}
+// fn test_complete_graph(n: usize, grundy_cache: &mut grundy_cache::GrundyCache) {
+//     let mut nodes = Vec::new();
+//     let mut graph: Graph<(), (), petgraph::Undirected> = Graph::new_undirected();
+//     for _ in 0..n {
+//         let node = graph.add_node(());
+//         nodes.push(node);
+//     }
+//     for i in 0..n - 1 {
+//         for j in i + 1..n {
+//             graph.add_edge(nodes[i], nodes[j], ());
+//         }
+//     }
+//     println!(
+//         "grundy of complete graph of size {} is {}",
+//         n,
+//         grundy::grundy(&graph, grundy_cache)
+//     );
+// }
 
 fn test_m_n(m: usize, n: usize, grundy_cache: &mut grundy_cache::GrundyCache) {
     let mut n_nodes = Vec::new();
