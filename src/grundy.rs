c2840215540d2b4f7c8027138cbdb9fd11af97f3
Huaiyuan Jing <ls.hyjing@gmail.com>
2025-03-03 22:15:22 -0600

    feat(grundy): add isolated node removal and debugging output in grundy function

--- src/grundy.rs
+++ src/grundy.rs
@@ -6,16 +6,21 @@ fn remove_isolated_nodes(
     g: &Graph<(), (), petgraph::Undirected>,
 ) -> Graph<(), (), petgraph::Undirected> {
     let mut tmp = g.clone();
-    let nodes_to_remove: Vec<_> = tmp
+    let mut nodes_to_remove: Vec<_> = tmp
         .node_indices()
-        .filter(|&node| g.neighbors(node).count() == 0)
+        .filter(|&node| tmp.neighbors(node).count() == 0)
         .collect();
+    nodes_to_remove.reverse();
     for node in nodes_to_remove {
         tmp.remove_node(node);
     }
     tmp
 }
-pub fn grundy(g: &Graph<(), (), petgraph::Undirected>, grundy_cache: &mut GrundyCache) -> u64 {
+pub fn grundy(
+    g: &Graph<(), (), petgraph::Undirected>,
+    grundy_cache: &mut GrundyCache,
+    show: bool,
+) -> u64 {
     let g = remove_isolated_nodes(g);
     if g.edge_count() == 0 {
         return 0;
@@ -28,17 +33,49 @@ pub fn grundy(g: &Graph<(), (), petgraph::Undirected>, grundy_cache: &mut Grundy
     for edge in g.edge_indices() {
         let mut tmp = g.clone();
         tmp.remove_edge(edge);
-        rev_mex.insert(grundy(&tmp, grundy_cache));
+        rev_mex.insert(grundy(&tmp, grundy_cache, show));
     }
     for node in g.node_indices() {
         let mut tmp = g.clone();
+        if g.neighbors(node).count() == 0 {
+            continue;
+        }
         tmp.remove_node(node);
-        rev_mex.insert(grundy(&tmp, grundy_cache));
+        rev_mex.insert(grundy(&tmp, grundy_cache, show));
     }
     let mut mex = 0;
     while rev_mex.contains(&mex) {
         mex += 1;
     }
     grundy_cache.insert(&g, mex);
+    if show {
+        println!(
+            "Node numbers {}, Edge numbers {}",
+            g.node_count(),
+            g.edge_count()
+        );
+        for node in g.node_indices() {
+            print!("{}'s neighbor: ", node.index());
+            for neighbor in g.neighbors(node) {
+                print!("{}, ", neighbor.index());
+            }
+            println!("");
+        }
+        println!("mex: {}", mex);
+    }
     mex
+}
+
+#[test]
+fn test_remove_isolated_nodes() {
+    let mut graph: Graph<(), (), petgraph::Undirected> = Graph::new_undirected();
+    for _ in 0..3 {
+        graph.add_node(());
+    }
+    let g = remove_isolated_nodes(&graph);
+    for node in g.node_indices() {
+        print!("{}: {} neighbors", node.index(), g.neighbors(node).count());
+    }
+    println!("");
+    assert_eq!(g.node_count(), 0);
 }
--- src/main.rs
+++ src/main.rs
@@ -6,54 +6,30 @@ use petgraph::graph::Graph;
 
 fn main() {
     let mut grundy_cache = grundy_cache::GrundyCache::new();
-    for i in 5..=5 {
-        for j in 1..=7 {
-            test_m_n(i, j, &mut grundy_cache);
+    for n in 1..=10 {
+        for m in 1..=10 {
+            let mut n_nodes = Vec::new();
+            let mut m_nodes = Vec::new();
+            let mut graph: Graph<(), (), petgraph::Undirected> = Graph::new_undirected();
+            for _ in 0..m {
+                let node = graph.add_node(());
+                m_nodes.push(node);
+            }
+            for _ in 0..n {
+                let node = graph.add_node(());
+                n_nodes.push(node);
+            }
+            for i in 0..m {
+                for j in 0..n {
+                    graph.add_edge(m_nodes[i], n_nodes[j], ());
+                }
+            }
+            println!(
+                "grundy of K_{}_{} is {}",
+                m,
+                n,
+                grundy::grundy(&graph, &mut grundy_cache, false)
+            );
         }
     }
-    println!("collision rate is {}%", grundy_cache.collision_rate());
-}
-
-// fn test_complete_graph(n: usize, grundy_cache: &mut grundy_cache::GrundyCache) {
-//     let mut nodes = Vec::new();
-//     let mut graph: Graph<(), (), petgraph::Undirected> = Graph::new_undirected();
-//     for _ in 0..n {
-//         let node = graph.add_node(());
-//         nodes.push(node);
-//     }
-//     for i in 0..n - 1 {
-//         for j in i + 1..n {
-//             graph.add_edge(nodes[i], nodes[j], ());
-//         }
-//     }
-//     println!(
-//         "grundy of complete graph of size {} is {}",
-//         n,
-//         grundy::grundy(&graph, grundy_cache)
-//     );
-// }
-
-fn test_m_n(m: usize, n: usize, grundy_cache: &mut grundy_cache::GrundyCache) {
-    let mut n_nodes = Vec::new();
-    let mut m_nodes = Vec::new();
-    let mut graph: Graph<(), (), petgraph::Undirected> = Graph::new_undirected();
-    for _ in 0..m {
-        let node = graph.add_node(());
-        m_nodes.push(node);
-    }
-    for _ in 0..n {
-        let node = graph.add_node(());
-        n_nodes.push(node);
-    }
-    for i in 0..m {
-        for j in 0..n {
-            graph.add_edge(m_nodes[i], n_nodes[j], ());
-        }
-    }
-    println!(
-        "grundy of K_{}_{} is {}",
-        m,
-        n,
-        grundy::grundy(&graph, grundy_cache)
-    );
 }
